9146588fe9a5b1e8e7036cc873df282f9974a5a3
CSharpFri <CSharpFri@CLASS-2-11>
2017-12-15 20:24:17 +0100

    comit

---
+++ Ruleta/Ruleta.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ruleta
+{
+    class Ruleta
+    {
+        public Hrac hrac;
+        public HraciStol hraciStol = new HraciStol();
+
+        public void Zahraj(Stavka stavka, Hrac hrac)
+        {
+            // vytoci cislo
+            //HraciePole Vytocenehp = VytocCislo()
+
+            //porovna ci vyhral ci nie 
+            //a upravi sa hracovi bank
+            //hrac.Bank +=  VratVyhru(stavka,Vytocenehp) 
+
+            // stavku zaznamena do historie
+        }
+    }
+}
---
+++ Ruleta/Stavka.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ruleta
+{
+    class Stavka
+    {
+        public int Vsadenaciastka;
+        public typstavky typstavky;
+        public Farba farba;
+        public Rada rada;
+        public Lichost lichost;
+    }
+}
